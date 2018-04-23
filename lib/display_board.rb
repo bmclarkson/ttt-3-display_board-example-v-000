f2e2d822fedcca1424c07cbc4e548b5362396962
Brass Clarkson <bm.clarkson@gmail.com>
2018-04-23 22:44:46 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,2 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "___________"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "___________"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-display_board(board)
+def display_board
+  puts "  "
